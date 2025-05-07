08223a71652b33818646344f4ee02180e69c6bb8
leonardo-luz <leonardoluz.f02@gmail.com>
2025-05-06 22:16:01 -0300

    feat: update player

--- src/objects/Player.tsx
+++ src/objects/Player.tsx
@@ -7,6 +7,7 @@ export class Player {
   private keys = useRef<{ [key: string]: boolean }>({})
   private speed = useRef(20)
   private timer = useRef(0)
+  private body = useState([{ x: 40, y: 40 }])
 
   public playerInput = () => {
     if (!this.spriteRef.current) return;
@@ -37,14 +38,35 @@ export class Player {
     this.timer.current += ticker.deltaMS
 
     if (this.timer.current > 200) {
-      if (direc == UP)
-        this.spriteRef.current.y -= this.speed.current * Math.round(ticker.deltaTime);
-      else if (direc == RIGHT)
-        this.spriteRef.current.x += this.speed.current * Math.round(ticker.deltaTime);
-      else if (direc == DOWN)
-        this.spriteRef.current.y += this.speed.current * Math.round(ticker.deltaTime);
-      else if (direc == LEFT)
-        this.spriteRef.current.x -= this.speed.current * Math.round(ticker.deltaTime);
+
+      let before: { x: number, y: number }
+
+      this.body[1](prev => {
+        const aux: { x: number, y: number }[] = [];
+        prev.forEach((part, index) => {
+          if (!this.spriteRef.current) return;
+
+          if (index == 0) {
+            if (direc == UP)
+              this.spriteRef.current.y -= this.speed.current * Math.round(ticker.deltaTime);
+            else if (direc == RIGHT)
+              this.spriteRef.current.x += this.speed.current * Math.round(ticker.deltaTime);
+            else if (direc == DOWN)
+              this.spriteRef.current.y += this.speed.current * Math.round(ticker.deltaTime);
+            else if (direc == LEFT)
+              this.spriteRef.current.x -= this.speed.current * Math.round(ticker.deltaTime);
+
+            before = { x: this.spriteRef.current.x, y: this.spriteRef.current.y }
+          }
+          const temp = part
+          part = before
+          before = temp
+
+          aux.push(part)
+        })
+
+        return aux
+      })
 
       this.timer.current = 0
     }
@@ -75,12 +97,22 @@ export class Player {
     }, [])
 
     useTick((ticker) => {
-      const aux = this.playerInput(ticker)
+      const aux = this.playerInput()
       setDirec(prev => {
         const UP = 1,
           RIGHT = 2,
           DOWN = 3,
           LEFT = 4
+
+        if (this.keys.current["v"])
+          alert(JSON.stringify(this.body[0]))
+
+        if (this.keys.current["i"])
+          this.body[1](prev => {
+            if (!this.spriteRef.current) return prev
+
+            return [{ x: this.spriteRef.current.x, y: this.spriteRef.current.y }, ...prev]
+          })
 
         if (!aux || (prev == UP && aux == DOWN) || (prev == DOWN && aux == UP) || (prev == LEFT && aux == RIGHT) || (prev == RIGHT && aux == LEFT))
           return prev
@@ -91,13 +123,22 @@ export class Player {
     })
 
     return (
-      <pixiSprite
-        ref={this.spriteRef}
-        texture={texture}
-        anchor={0.5}
-        x={40}
-        y={40}
-      />
+      this.body[0].map(part => {
+        const sprite = <pixiSprite
+          ref={this.spriteRef}
+          texture={texture}
+          anchor={0.5}
+          x={part.x}
+          y={part.y}
+        />
+
+        // if (this.spriteRef.current) {
+        //   this.spriteRef.current.x = part.x
+        //   this.spriteRef.current.y = part.y
+        // }
+
+        return sprite
+      })
     );
   }
 }
